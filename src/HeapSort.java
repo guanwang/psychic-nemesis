94da49c3f5c8c622ce4dc790ffd8fbb47a65f6c9
ewanggu <guan.g.wang@ericsson.com>
2015-07-10 13:18:24 +0800

    update

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -21,9 +21,10 @@ public class HeapSort {
 
     private void heapSort(final int[] array) {
         buildHeap(array);
-        for (int i = array.length - 1; i > 1; i--) {
+        for (int i = array.length - 1; i > 0; i--) {
             swap(array, 0, i);
-            heapify(array, 0, i - 1);
+            heapify(array, 0, i);
+
         }
     }
 
